cda0356c1a7f17260e025b4ef4d70e8ec7817e2f
Michael Martin <mwm.cal@gmail.com>
2015-05-07 11:36:01 -0700

    kill it first

--- docker-up.sh
+++ docker-up.sh
@@ -1,3 +1,7 @@
+git pull origin
+
+docker stop postgres
+
 docker build -t postgres-test .
 
-docker run --name postgres -e POSTGRES_PASSWORD=LifeTime1 -p 5432:5432 -d postgres-test
+docker run --rm --name postgres -e POSTGRES_PASSWORD=LifeTime1 -p 5432:5432 -d postgres-test

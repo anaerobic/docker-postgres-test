2b8246da0f44876394cea97843cf43ae6462693c
Michael Martin <mwm.cal@gmail.com>
2015-05-07 11:37:22 -0700

    fixing --rm conflict with -d (whoops)

--- docker-up.sh
+++ docker-up.sh
@@ -2,6 +2,8 @@ git pull origin
 
 docker stop postgres
 
+docker rm postgres
+
 docker build -t postgres-test .
 
-docker run --rm --name postgres -e POSTGRES_PASSWORD=LifeTime1 -p 5432:5432 -d postgres-test
+docker run --name postgres -e POSTGRES_PASSWORD=LifeTime1 -p 5432:5432 -d postgres-test

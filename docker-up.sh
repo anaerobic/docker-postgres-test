1366dbe4e22514b71029bdc2c02d009ecc14bdf7
Michael Martin <mwm.cal@gmail.com>
2015-05-19 12:11:28 -0700

    Updating docker-up.sh

--- docker-up.sh
+++ docker-up.sh
@@ -6,4 +6,4 @@ docker rm postgres
 
 docker build -t postgres-test .
 
-docker run --name postgres -h postgres -e POSTGRES_PASSWORD=LifeTime1 -p 5432:5432 -d postgres-test
+docker run --name postgres -h postgres.lacolhost.com -e POSTGRES_PASSWORD=LifeTime1 -p 5432:5432 -d postgres-test
